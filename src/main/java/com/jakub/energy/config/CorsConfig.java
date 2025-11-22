e411d3e2036e2db03ff8bafe56dc9ddaef10b278
kubula316 <161055506+kubula316@users.noreply.github.com>
2025-11-22 21:28:16 +0100

    -changed cors

--- src/main/java/com/jakub/energy/config/CorsConfig.java
+++ src/main/java/com/jakub/energy/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("cors.allowed.origin")
+                        .allowedOrigins("${cors.allowed.origins}")
                         .allowedMethods("GET")
                         .allowedHeaders("Content-Type", "Accept");
             }
