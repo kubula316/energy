85a9dc48a2c0ef161d40ab2849c46e30a65710da
kubula316 <161055506+kubula316@users.noreply.github.com>
2025-11-22 21:30:22 +0100

    -changed cors

--- src/main/java/com/jakub/energy/config/CorsConfig.java
+++ src/main/java/com/jakub/energy/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.jakub.energy.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,10 +13,13 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
 
+            @Value("${cors.allowed-origins}")
+            private String[] allowedOriginsArray;
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("${cors.allowed.origins}")
+                        .allowedOrigins(allowedOriginsArray)
                         .allowedMethods("GET")
                         .allowedHeaders("Content-Type", "Accept");
             }
